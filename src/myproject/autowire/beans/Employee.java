143993ff0e6e1ff91db69b58bb6d35cfcae96008
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-01-12 10:41:41 +0530

    Constructor Dependency Injection

--- src/myproject/autowire/beans/Employee.java
+++ src/myproject/autowire/beans/Employee.java
@@ -7,37 +7,49 @@ public class Employee {
 	private float esal;
 	private Account account;
 	private Address address;
-	public String getEid() {
-		return eid;
-	}
-	public void setEid(String eid) {
+	
+	
+public Employee(String eid, String ename, float esal, Account account, Address address) {
 		this.eid = eid;
-	}
-	public String getEname() {
-		return ename;
-	}
-	public void setEname(String ename) {
 		this.ename = ename;
-	}
-	public float getEsal() {
-		return esal;
-	}
-	public void setEsal(float esal) {
 		this.esal = esal;
-	}
-	public Account getAccount() {
-		return account;
-	}
-	public void setAccount(Account account) {
 		this.account = account;
-	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
 		this.address = address;
 	}
-	
+
+
+//	
+//	public String getEid() {
+//		return eid;
+//	}
+//	public void setEid(String eid) {
+//		this.eid = eid;
+//	}
+//	public String getEname() {
+//		return ename;
+//	}
+//	public void setEname(String ename) {
+//		this.ename = ename;
+//	}
+//	public float getEsal() {
+//		return esal;
+//	}
+//	public void setEsal(float esal) {
+//		this.esal = esal;
+//	}
+//	public Account getAccount() {
+//		return account;
+//	}
+//	public void setAccount(Account account) {
+//		this.account = account;
+//	}
+//	public Address getAddress() {
+//		return address;
+//	}
+//	public void setAddress(Address address) {
+//		this.address = address;
+//	}
+//	
 	public void getEmpDetails() {
 		System.out.println("Employee Details");
 		System.out.println("-------------------------");
